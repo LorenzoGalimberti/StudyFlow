321c0f24d297bd1a288a92c5483be6a93c37ddc8
LorenzoGalimberti <lorenzogalimberti7996@gmail.com>
2025-09-19 17:32:50 +0200

    Add firebase config for build

---
+++ src/services/firebase-config.ts
@@ -0,0 +1,16 @@
+// Configurazione Firebase
+// IMPORTANTE: Questo file è incluso nel .gitignore per sicurezza
+
+import { FirebaseOptions } from 'firebase/app';
+
+export const firebaseConfig: FirebaseOptions = {
+  apiKey: "AIzaSyDlA31_rvig5t5K71N2pxXZ2vDB_d1e8eA",
+  authDomain: "knowhow-e6565.firebaseapp.com",
+  databaseURL: "https://knowhow-e6565-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "knowhow-e6565",
+  storageBucket: "knowhow-e6565.firebasestorage.app",
+  messagingSenderId: "640618942053",
+  appId: "1:640618942053:web:c8044af364f1f597ea808c"
+};
+
+// Sostituisci tutti i valori sopra con la tua configurazione Firebase!
